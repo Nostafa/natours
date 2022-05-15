c9e0d437dd5052d83b748c75f66d4bb15aa0b0cb
Mustafa Yasser Soudi <40180893+Nostafa@users.noreply.github.com>
2022-05-15 23:36:16 +0200

    pug file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -15,6 +16,9 @@ const userRouter = require('./router/user.routes');
 const tourRouter = require('./router/tour.routes');
 const reviewRouter = require('./router/review.routes');
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 // 1) Middleware
 
 // Set security HTTP Headers
